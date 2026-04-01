b40f6d4bdef5af21d1c8a3d3fa13d6f15686307b
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-04-01 19:26:22 +0530

    Dock created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./app.scss"
+import Dock from './components/Dock'
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Dock/>
+      </main>
+    </div>
+  )
+}
+
+export default App
