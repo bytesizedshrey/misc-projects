28696b1a4e4f06f1af11c9d6c4ab8d9080e59f40
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-08-10 17:05:51 +0530

    style: remove red badge from email button

--- localhostshrey/app/(root)/(home)/page.tsx
+++ localhostshrey/app/(root)/(home)/page.tsx
@@ -255,7 +255,7 @@ export default function Home() {
           </FatLink>
           , or{" "}
           <button
-            className="inline-flex cursor-pointer bg-[#ef4444] text-white px-1"
+            className="inline-flex cursor-pointer"
             onClick={handleEmailClick}
             onPointerEnter={() => playAudio("tick")}
             onPointerDown={() => playAudio("press")}
